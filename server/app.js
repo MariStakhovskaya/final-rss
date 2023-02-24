d4f687dddcfbcadd8183f0ee6a94ee5f53d78d87
Mari Stakhovskaya <maryia.jegorova@gmail.com>
2023-02-24 21:11:27 +0300

    refactor: change origin

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const whitelist = [
 
 const corsOptions = {
    //origin: "http://localhost:3000",
-   origin: "https://MariStakhovskaya.github.io/final-rss",
+   origin: ["http://localhost:3000", "https://MariStakhovskaya.github.io"],
    //origin: "*",
    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
    Headers: '*',
